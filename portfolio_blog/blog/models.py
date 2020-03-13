c08facdd9630601a58939df0fd3f5c5a367adf50
William Chrapcynski <wchrapcynski@gmail.com>
2020-03-13 14:47:09 -0400

    Set up models

--- portfolio_blog/blog/models.py
+++ portfolio_blog/blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comment(self):
+        return self.comments.filter(approve_comment=True)
+
+    # Go here after post is created
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'blog.Post', related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approve_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+    def __str__(self):
+        return self.text
